09c9839de4eb9fbe3a86de53cf94f069efeba18b
Daniel Cowpertwait <daniel.cowpertwait@wework.com>
2019-08-23 05:23:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,11 +7,11 @@ subtraction = num1 - num2
 end
 
 def division(num1, num2)
-
+division = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+multiplication = num1 * num2
 end
 
 def modulo(num1, num2)
